530d0f1fd4b803777c1e71420b96b9ae21924071
Miguel Raatz <miguelraatz_@hotmail.com>
2023-01-20 15:47:48 -0300

    realizado testes requisito 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { FavoritePokemon } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste do componente favoritePokemon', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
+    renderWithRouter(<FavoritePokemon />);
+    const messageNoFoundPokemon = screen.getByText(/no favorite pokémon found/i);
+    expect(messageNoFoundPokemon).toBeInTheDocument();
+  });
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDetails);
+    const checkFavorite = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    userEvent.click(checkFavorite);
+    const linkFavorite = screen.getByRole('link', { name: /favorite pokémon/i });
+    userEvent.click(linkFavorite);
+    const isFavorite = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
+    expect(isFavorite).toBeInTheDocument();
+  });
+});
